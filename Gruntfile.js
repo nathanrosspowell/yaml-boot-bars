c045b414a17b7e42295b4f7b080b4b0e71abddb3
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-06-14 01:20:12 -0400

    Working flow
    * All of the data is going from src to build to being deployed

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,14 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+var fs = require('fs');
+var path = require('path');
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-replace');
+
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
@@ -10,26 +17,26 @@ module.exports = function(grunt) {
             drop_console: true
         }
       },
-      my_targets: {
+      ybb: {
         files: {
             'build/js/main.js': ['src/js/main.js'],
             'build/js/scripts.js': ['src/js/scripts.js'],
-            'build/js/the_data.js': ['src/js/the_data.js'],
         }
       }
     },
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     copy: {
-      my_targets: {
+      ybb: {
         files: [
           {
             expand:true,
             cwd: "src/",
             src: [
-                '**/*', // The full source tree
-                '!**/js/main.js', // Ignore
-                '!**/js/scripts.js', // Ignore
-                '!**/js/the_data.js', // Ignore
-                '!**/yaml/*', // Ignore
+                '**/*',                 // The full source tree, but...
+                '!**/index.handlebars', // ignore this,
+                '!**/js/main.js',       // ignore this,
+                '!**/js/scripts.js',    // ignore this,
+                '!**/yaml/*',           // and ignore this folder.
             ],
             dest: 'build/',
             filter: "isFile"
@@ -37,8 +44,9 @@ module.exports = function(grunt) {
         ]
       }
     },
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     yaml: {
-      my_targets: {
+      ybb: {
         options: {
           space: 2
         },
@@ -52,34 +60,67 @@ module.exports = function(grunt) {
         ]
       }
     },
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     concat: {
-      my_targets: {
+      ybb: {
         options: {
           process: function(src, filepath) {
             var filename = filepath.replace(/^.*[\\\/]/, '')
             var withoutExt = filename.split('.')[ 0 ]
-            return "'"+ withoutExt + "': " +
-              src.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
+            return '"'+ withoutExt + '": ' + src + ",";
           },
         },
         files: {
-          'temp/built.js': [ 'temp/json/*.json' ]
+          'temp/concatfile.json': [ 'temp/json/*.json' ]
         },
       }
     },
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    replace : {
+      ybb: {
+        options: {
+          patterns: [{
+            match: /\},$/g, // This is the last comma in the file
+            replacement: "}",
+          }]
+        },
+        files: {
+            'temp/concatfile.json':'temp/concatfile.json' 
+        },
+      }
+    },
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     file_append: {
-      my_targets: {
+      ybb: {
         files: {
           'temp/built.json': {
             prepend: "{\n",
-            append: "\n}",
-            input: 'temp/built.js'
+            append: "\n}\n",
+            input: 'temp/concatfile.json'
           }
         },
       }
     },
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    'compile-handlebars': {
+      ybb: {
+        template: 'src/index.handlebars',
+        templateData: 'temp/built.json',
+        output: 'build/index.html'
+      }
+    },
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    prettify: {
+      ybb: {
+        options: {
+          json: ['temp/built.json']
+        },
+        files: { 'build/index.html' : 'build/index.html' }
+      }
+    },
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     'gh-pages': {
-        my_targets: {
+        ybb: {
           options: {
             base: 'build'
           },
@@ -87,17 +128,24 @@ module.exports = function(grunt) {
         }
     }
   });
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Load tasks.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-static-handlebars');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-yaml');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-file-append');
   grunt.loadNpmTasks('grunt-gh-pages');
+  grunt.loadNpmTasks('grunt-compile-handlebars');
+  grunt.loadNpmTasks('grunt-static-handlebars');
+  grunt.loadNpmTasks('grunt-replace');
+  grunt.loadNpmTasks('grunt-prettify');
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Default task: move everything to the 'build' folder.
-  grunt.registerTask('default', ['uglify','copy']);
-  grunt.registerTask('make-yaml', ['yaml','concat', 'file_append']);
+  grunt.registerTask('default', ['make-yaml', 'handlebars','uglify', 'copy']);
+  grunt.registerTask('make-yaml', ['yaml','concat', 'replace', 'file_append']);
+  grunt.registerTask('handlebars', ['compile-handlebars', 'prettify']);
   // deploy task: runs everything in order.
-  grunt.registerTask('deploy', ['uglify','copy', 'gh-pages']);
+  grunt.registerTask('deploy', ['default', 'gh-pages']);
 };
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
