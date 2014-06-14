69519c734c2881d69476243cbfc8e827fea78910
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-06-13 21:32:25 -0400

    Working YAML -> JSON grunt task
    * All yaml files in the folder are turned into json
    * All the json files are concatanated into one big file with their
    filename as the key and the file content as the value
    * That partial file is passed to file_append to put the opening and closing
    brackets to make the built.json file

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,6 +29,7 @@ module.exports = function(grunt) {
                 '!**/js/main.js', // Ignore
                 '!**/js/scripts.js', // Ignore
                 '!**/js/the_data.js', // Ignore
+                '!**/yaml/*', // Ignore
             ],
             dest: 'build/',
             filter: "isFile"
@@ -36,17 +37,67 @@ module.exports = function(grunt) {
         ]
       }
     },
-    'gh-pages': {
+    yaml: {
+      my_targets: {
         options: {
-            base: 'build'
+          space: 2
         },
-        src: ['**']
+        files: [
+            {
+              expand: true,
+              cwd: 'src/yaml/',
+              src: ['**/*.yml'],
+              dest: 'temp/json/'
+            }
+        ]
+      }
+    },
+    concat: {
+      my_targets: {
+        options: {
+          process: function(src, filepath) {
+            var filename = filepath.replace(/^.*[\\\/]/, '')
+            var withoutExt = filename.split('.')[ 0 ]
+            return "'"+ withoutExt + "': " +
+              src.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
+          },
+        },
+        files: {
+          'temp/built.js': [ 'temp/json/*.json' ]
+        },
+      }
+    },
+    file_append: {
+      my_targets: {
+        files: {
+          'temp/built.json': {
+            prepend: "{\n",
+            append: "\n}",
+            input: 'temp/built.js'
+          }
+        },
+      }
+    },
+    'gh-pages': {
+        my_targets: {
+          options: {
+            base: 'build'
+          },
+          src: ['**']
+        }
     }
   });
   // Load tasks.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-static-handlebars');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-yaml');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-file-append');
   grunt.loadNpmTasks('grunt-gh-pages');
-  // Default task(s).
+  // Default task: move everything to the 'build' folder.
   grunt.registerTask('default', ['uglify','copy']);
+  grunt.registerTask('make-yaml', ['yaml','concat', 'file_append']);
+  // deploy task: runs everything in order.
+  grunt.registerTask('deploy', ['uglify','copy', 'gh-pages']);
 };
