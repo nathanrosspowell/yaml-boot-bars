1a64196204cd3ea42b96a0d621699a22fbf8eae7
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-06-14 01:16:45 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-/*! yaml-boot-bars - v0.0.1 - 2014-06-13 */function applyTemplate(a,b,c){var d=$("#"+b+"-template").html(),e=Handlebars.compile(d);$(a).append(e(c))}$(window).bind("load",function(){applyTemplate("#navList","navList",theData),applyTemplate("#sections","sections",theData)});
+/*! yaml-boot-bars - v0.0.1 - 2014-06-14 */
